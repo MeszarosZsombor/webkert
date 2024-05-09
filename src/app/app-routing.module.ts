2ec54240d13b786939c95947174fd9a69bdea512
MeszarosZsombor <mzsombor10@gmail.com>
2024-05-10 00:06:42 +0200

    navbar, menubar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,19 +6,31 @@ const routes: Routes = [
       path: 'not-found',
       loadChildren: () => import('./pages/not-found/not-found.module').then(m => m.NotFoundModule)
     },
-  {
-    path: 'main',
-    loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule)
-  },
-  {
-    path: '',
-    redirectTo: '/main',
-    pathMatch: "full"
-  },
-  {
-    path: '**',
-    redirectTo: '/not-found'
-  }
+    {
+      path: 'login',
+      loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+    },
+    {
+      path: 'register',
+      loadChildren: () => import('./pages/register/register.module').then(m => m.RegisterModule)
+    },
+    {
+      path: 'account',
+      loadChildren: () => import('./pages/account/account.module').then(m => m.AccountModule)
+    },
+    {
+      path: 'main',
+      loadChildren: () => import('./pages/main/main.module').then(m => m.MainModule)
+    },
+    {
+      path: '',
+      redirectTo: '/main',
+      pathMatch: "full"
+    },
+    {
+      path: '**',
+      redirectTo: '/not-found'
+    }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,34 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatIcon} from "@angular/material/icon";
+import { MenuComponent } from './shared/menu/menu/menu.component';
+import {MatListItem, MatNavList} from "@angular/material/list";
+import {MatButtonModule} from "@angular/material/button";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIcon,
+    MatNavList,
+    MatButtonModule,
+    MatListItem
   ],
   providers: [
-    provideClientHydration()
+    provideClientHydration(),
+    provideAnimationsAsync()
   ],
   bootstrap: [AppComponent]
 })
